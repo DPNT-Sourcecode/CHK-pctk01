51aec5e822ad23a95280c5b9c3bc454fba804677
sbx_user1051 <sbx_user1051@169.254.7.169>
2023-11-22 20:30:24 +0000

    Wed Nov 22 20:30:24 UTC 2023

--- src/BeFaster.App/Solutions/CHK/Parse.cs
+++ src/BeFaster.App/Solutions/CHK/Parse.cs
@@ -2,11 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BeFaster.App.Solutions.CHK
 {
-    internal class Parse
+    public static class Parse
     {
+        public static Dictionary<char, int> ParseSkus(string skus)
+        {
+            if (String.IsNullOrEmpty(skus) || String.IsNullOrWhiteSpace(skus))
+            {
+                throw new NullReferenceException("Invalid input.");
+            }
+
+            string pattern = @"[A-Z]+";
+
+            var regex = new Regex(pattern);
+
+            Match match = regex.Match(skus);
+
+            if (match.Success)
+            {
+                var items = new Dictionary<char, int>();
+
+                foreach (char it in skus)
+                {
+                    if (items.ContainsKey(it))
+                    {
+                        items[it]++;
+                    }
+                    else
+                    {
+                        items.Add(it, 1);
+                    }
+                }
+
+                return items;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+        }
     }
 }
+
