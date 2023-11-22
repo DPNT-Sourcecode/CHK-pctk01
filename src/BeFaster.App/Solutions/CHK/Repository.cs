5e8304e03131a3d6f5c9de3d126c89ae692bcbab
sbx_user1051 <sbx_user1051@169.254.7.169>
2023-11-22 21:03:42 +0000

    Wed Nov 22 21:03:42 UTC 2023

--- src/BeFaster.App/Solutions/CHK/Repository.cs
+++ src/BeFaster.App/Solutions/CHK/Repository.cs
@@ -17,7 +17,8 @@ namespace BeFaster.App.Solutions.CHK
                 new Item("B", 30),
                 new Item("C", 20),
                 new Item("D", 15),
-                new Item("E", 40)
+                new Item("E", 40),
+                new Item("F", 10)
             };
         }
 
@@ -27,7 +28,7 @@ namespace BeFaster.App.Solutions.CHK
             {
                 new SpecialOfferPrice(items.Find(i => i.Name.Equals("A")), 3, 130),
                 new SpecialOfferPrice(items.Find(i => i.Name.Equals("B")), 2, 45),
-                new SpecialOfferPrice(items.Find(i => i.Name.Equals("A")), 5, 200)
+                new SpecialOfferPrice(items.Find(i => i.Name.Equals("A")), 5, 200),
             };
         }
 
@@ -35,8 +36,10 @@ namespace BeFaster.App.Solutions.CHK
         {
             return new List<SpecialOfferItem>()
             {
-                new SpecialOfferItem(items.Find(i => i.Name.Equals("E")), 2, items.Find(i => i.Name.Equals("B")))
+                new SpecialOfferItem(items.Find(i => i.Name.Equals("E")), 2, items.Find(i => i.Name.Equals("B"))),
+                new SpecialOfferItem(items.Find(i => i.Name.Equals("F")), 2, items.Find(i => i.Name.Equals("F")))
             };
         }
     }
 }
+
