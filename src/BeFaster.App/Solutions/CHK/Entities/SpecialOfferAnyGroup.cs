4856fe82d3d7ddd53f4c4c0aeefba7be8425c2ed
sbx_user1051 <sbx_user1051@169.254.7.169>
2023-11-22 21:36:08 +0000

    Wed Nov 22 21:36:08 UTC 2023

---
+++ src/BeFaster.App/Solutions/CHK/Entities/SpecialOfferAnyGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK.Entities
+{
+    internal class SpecialOfferAnyGroup
+    {
+        public List<Item> Items { get; set; }
+        public int Quantity { get; set; }
+        public int TotalPrice { get; set; }
+    }
+}
