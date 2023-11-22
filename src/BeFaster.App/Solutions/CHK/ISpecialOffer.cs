8b36cf27b653b1366a529e64b252020202d244d1
sbx_user1051 <sbx_user1051@169.254.7.169>
2023-11-22 20:18:24 +0000

    Wed Nov 22 20:18:24 UTC 2023

---
+++ src/BeFaster.App/Solutions/CHK/ISpecialOffer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    public interface ISpecialOffer
+    {
+        Item ItemOffer { get; set; }
+        int Quantity { get; set; }
+    }
+}
---
+++ src/BeFaster.App/Solutions/CHK/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFaster.App.Solutions.CHK
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Item()
+        {
+            Name = string.Empty;
+            Price = 0;
+        }
+
+        public Item(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
